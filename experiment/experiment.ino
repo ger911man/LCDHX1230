17b24d2d1325b9e1d0ad6c337b0bd700c2012d8d
ger911man@gmail.com <ger911man@gmail.com>
2020-01-25 09:17:32 -0500

    created HX1230 demo

--- experiment/experiment.ino
+++ experiment/experiment.ino
@@ -1,20 +1,49 @@
-#include "Arduino.h"
-int led = 13;
-int voltTest = 12;
+// HX1230 library example
+// (c) 2019 Pawel A. Hernik
 
-// the setup routine runs once when you press reset:
-void setup() {
-    // initialize the digital pin as an output.
-    pinMode(led, OUTPUT);
-    pinMode(voltTest, OUTPUT);
+/*
+  HX1230 96x68 LCD connections (header on bottom, from left):
+  #1 RST - D6 or any digital
+  #2 CE  - D7 or any digital
+  #3 N/C
+  #4 DIN - D11/MOSI
+  #5 CLK - D13/SCK
+  #6 VCC - 3V3
+  #7 BL  - 3V3 or any digital
+  #8 GND - GND
+*/
+
+#include "Arduino.h"
+
+#define LCD_RST 6
+#define LCD_CS  7
+#define LCD_BL  8
+#define LCD_DIN 11
+#define LCD_CLK 13
+
+#include "HX1230_SPI.h"
+// define USESPI in above header for HW 9-bit SPI
+
+//HX1230_SPI lcd(LCD_RST, LCD_CS, LCD_DIN, LCD_CLK);
+HX1230_SPI lcd(LCD_RST, LCD_CS);
+
+#if USESPI==1
+#include <SPI.h>
+#endif
+
+#include "term8x14_font.h"
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(LCD_BL, OUTPUT);
+    digitalWrite(LCD_BL, HIGH);
+    lcd.init();
+    lcd.clrScr();
+    lcd.setFont(Term8x14PL);
+    lcd.printStr(3, 3, "Hello World");
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-    digitalWrite(voltTest, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(2000);               // wait for a second
-    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-    digitalWrite(voltTest, LOW);    // turn the LED off by making the voltage LOW
-    delay(2000);               // wait for a second
+void loop()
+{
 }
