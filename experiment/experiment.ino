4ca606d6c60c1c9b61ea456dd2aa2ac05bd1a7b9
ger911man@gmail.com <ger911man@gmail.com>
2020-01-25 19:25:37 -0500

    grayscale demo

--- experiment/experiment.ino
+++ experiment/experiment.ino
@@ -1,4 +1,4 @@
-// HX1230 library example
+// HX1230 96x68 LCD FB library example
 // (c) 2019 Pawel A. Hernik
 
 /*
@@ -13,25 +13,19 @@
   #8 GND - GND
 */
 
-#include "Arduino.h"
-
 #define LCD_RST 6
 #define LCD_CS  7
 #define LCD_BL  8
-#define LCD_DIN 11
-#define LCD_CLK 13
 
-#include "HX1230_SPI.h"
-// define USESPI in above header for HW 9-bit SPI
-
-//HX1230_SPI lcd(LCD_RST, LCD_CS, LCD_DIN, LCD_CLK);
-HX1230_SPI lcd(LCD_RST, LCD_CS);
-
-#if USESPI==1
+#include "HX1230_FB.h"
 #include <SPI.h>
-#endif
+HX1230_FB lcd(LCD_RST, LCD_CS);
 
+// from PropFonts library
+#include "c64enh_font.h"
 #include "term8x14_font.h"
+
+char buf[20];
 
 void setup()
 {
@@ -39,26 +33,66 @@ void setup()
     pinMode(LCD_BL, OUTPUT);
     digitalWrite(LCD_BL, HIGH);
     lcd.init();
-    lcd.clrScr();
-    lcd.setFont(Term8x14PL);
-    lcd.printStr(3, 3, "Hello World");
+    lcd.cls();
+    int dx = SCR_WD/16;
+    for(int j=1;j<17;j++) { lcd.setDither(j); lcd.fillRectD((j-1)*dx,0,dx,SCR_HT,1); }
+    lcd.display();
+    delay(500);
 }
 
-uint8_t x = 0, y = 0, contrast = 0;
+void sample(int s)
+{
+    lcd.cls();
+    lcd.setDither(s);
+    lcd.setFont(c64enh);
+    snprintf(buf,20,"Pattern: %02d",s);
+    lcd.printStr(ALIGN_CENTER, 2, buf);
+    lcd.fillRectD(6,13,44,32,1);
+    lcd.fillCircleD(96-24,28,16,1);
+    int dx = SCR_WD/16;
+    for(int j=1;j<17;j++) {
+        lcd.setFont(c64enh);
+        buf[0]=(j>9) ? j-10+'A' : j+'0';
+        buf[1]=0;
+        lcd.printStr(2+(j-1)*dx, SCR_HT-8*2, buf);
+        lcd.setDither(j);
+        lcd.fillRectD((j-1)*dx,SCR_HT-8,dx,8,1);
+    }
+    lcd.display();
+    delay(500);
+}
+
 uint8_t frameCounter = 0;
+float x=22, y=33;
+float dx = -0.3, dy = -0.3;
 void loop()
 {
-    lcd.clrScr();
-    lcd.printStr(3, 1, "Hello World");
+    lcd.cls();
     frameCounter++;
-    if(frameCounter>1){
-        frameCounter = 0;
-        lcd.printStr(3, 3, "DOOD");
+    if(frameCounter%2){
+        lcd.setDither(8);
+    } else {
+        lcd.setDither(-8);
     }
+    lcd.fillTriangleD(0,0,80,20,50,60,1);
+    if( x<=20 || x>=SCR_WD-20 ) dx=-dx;
+    if( y<=20 || y>=SCR_HT-20 ) dy=-dy;
+    x+=dx;
+    y+=dy;
+    lcd.fillCircle(x,y,15,1);
 
-//    lcd.setContrast(contrast);
-//    if(contrast>=31) contrast=0;
-//    if(x>80) x=0;
-//    if(y>5) y=0;
-    delay(10);
+    lcd.display();
+//    for(int i=0;i<=16;i++){
+//        lcd.cls();
+//        lcd.setDither(i);
+//        lcd.drawLineD(0,20,i*2,1);
+//        lcd.fillRectD(0+5*i,0,5,50,1);
+//        lcd.fillTriangleD(0,0,40,40,60,15,1);
+//        lcd.display();
+//        delay(50);
+//    }
+
+//    for(int i=0;i<=16;i++) sample(i);
+//    for(int i=16;i>0;i--) sample(i);
 }
+
